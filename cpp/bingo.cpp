48bb6636c5dc8729a9b0fa02b69fe3df63e983e0
ScavazzaRian <rianscavazza.git@gmail.com>
2024-12-05 14:57:11 -0300

    Criando funções básicas da aplicação
    - Gerando números aleatório;
    - Populando vetores letras e garantindo que sejam únicos;

--- cpp/bingo.cpp
+++ cpp/bingo.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
-//Biblioteca do C++ utlizada pra gerar os números aleatórios
+// Biblioteca do C++ utlizada pra gerar os números aleatórios
 #include <cstdlib>
-//Utilizada ara iniciar a semente do gerador de números com base no horário.
+// Utilizada ara iniciar a semente do gerador de números com base no horário.
 #include <ctime>
 #define MAX_CARTELA 10
 
 using namespace std;
+
+int gerarAleatorio(int min, int max);
+void preencheCartelaBingo(numeroBingo *vetor, int min, int max);
 
 struct numeroBingo
 {
@@ -26,12 +29,52 @@ int main()
 
     do
     {
-        cout << "======================" << endl;
-        cout << "BEM VINDO AO BINGO CPP" << endl;
-        cout << "Escolha uma das opcoes a seguir" << endl;
-        cout << "1 - Criar cartelas \n2 - Jogar \n3 - Sair" << endl;
+        cout << "|=================================|" << endl;
+        cout << "|      BEM VINDO AO BINGO CPP     |" << endl;
+        cout << "|=================================|" << endl;
+        cout << "| Escolha uma das opcoes a seguir:|" << endl;
+        cout << "| 1 - Criar cartelas              |" << endl;
+        cout << "| 2 - Jogar                       |" << endl;
+        cout << "| 3 - Sair                        |" << endl;
+        cout << "|=================================|" << endl;
         cin >> opcao;
-    } while(opcao != 3);
+
+        switch (opcao)
+        {
+        case 1:
+            cout << "Cartelas cadastradas com sucesso!!" << endl;
+            break;
+        }
+    } while (opcao != 3);
 
     return 0;
+}
+
+int gerarAleatorio(int min, int max)
+{
+    return min + rand() % (max - min + 1);
+}
+
+void preencheCartelaBingo(numeroBingo *vetor, int min, int max)
+{
+    int i = 0;
+    // Tive que fazer um while já que só posso incrementar se não houver número repetido no vetor
+    while (i < 5)
+    {
+        int num = gerarAleatorio(min, max);
+        bool repetido = false;
+        /*Comparo cada elemento gerado até j < i. Como eu só incremento o i quando os valores são únicos, caso os números sejam iguais ele irá cair no if e irá reiniciar o ciclo até gerar um número único.*/
+        for (int j = 0; j < i; j++)
+        {
+            if (vetor[j].valor == num)
+            {
+                repetido = true;
+                break;
+            }
+        }
+        // Por fim caso o número não seja repitido eu incremento o i e adiciono um valor a ele.
+        vetor[i].valor = num;
+        vetor[i].marcado = false;
+        i++;
+    }
 }
